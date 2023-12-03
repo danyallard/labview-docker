910829f823030d91091020112998828b66772c46
danyallard <dany.allard@videotron.ca>
2023-12-03 18:26:23 -0500

    Add VIAN UTF

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # LabVIEW Docker
 
-This repo contains docker files for building LabVIEW Docker images
+This repo contains docker files for building LabVIEW 2021 and 2022 Docker images
+
+LabVIEW 2023 not supported yet due to error installing the latest NI Package Manager
 
 ## Docker Hierarchy
 
 ```mermaid
 graph TB
-NIPM --> LabVIEW
-LabVIEW --> VIAnalyzer
-LabVIEW --> UTF
+ID-1[NIPM]
+ID-2[LabVIEW 202x]
+ID-3[VI Analyzer]
+ID-4[Unit Test Framework]
+ID-1 --> ID-2
+ID-2 --> ID-3
+ID-2 --> ID-4
 ```
--- build_labview_docker.ps1
+++ build_labview_docker.ps1
@@ -2,11 +2,14 @@
 $src_folder="src"
 $nipm_version="22.8"
 $labview_version_21="21.0"
-#$labview_version_22="22.3"
+$labview_version_22="22.3"
+#$labview_version_23="23.3"
 $bitness_32="32bit"
-#$bitness_64="64bit"
-$vianalyzer_version="21.0"
-$utf_version="21.0"
+$bitness_64="64bit"
+$vianalyzer_version_21="21.0"
+$vianalyzer_version_22="22.3"
+$utf_version_21="21.0"
+$utf_version_22="22.3"
 $docker_user="danyallard"
 
 $package_name_list = [System.Collections.ArrayList]::new()
@@ -17,17 +20,29 @@ $package_name_list.Add("utf")
 
 $labview_version_list = [System.Collections.ArrayList]::new()
 $labview_version_list.Add($labview_version_21)
-#$labview_version_list.Add($labview_version_22)
+$labview_version_list.Add($labview_version_22)
+#$labview_version_list.Add($labview_version_23)
+
+$vianalyzer_version_list = [System.Collections.ArrayList]::new()
+$vianalyzer_version_list.Add($vianalyzer_version_21)
+$vianalyzer_version_list.Add($vianalyzer_version_22)
+#$vianalyzer_version_list.Add($vianalyzer_version_23)
+
+$utf_version_list = [System.Collections.ArrayList]::new()
+$utf_version_list.Add($utf_version_21)
+$utf_version_list.Add($utf_version_22)
+#$utf_version_list.Add($utf_version_23)
+
 
 $package_bitness_list = [System.Collections.ArrayList]::new()
 $package_bitness_list.Add($bitness_32)
-#$package_bitness_list.Add($bitness_64)
+$package_bitness_list.Add($bitness_64)
 
 $package_version_list = [System.Collections.ArrayList]::new()
 $package_version_list.Add($nipm_version)
 $package_version_list.Add($labview_version_list)
-$package_version_list.Add($vianalyzer_version)
-$package_version_list.Add($utf_version)
+$package_version_list.Add($vianalyzer_version_list)
+$package_version_list.Add($utf_version_list)
 
 Clear-Host
 
@@ -37,10 +52,10 @@ foreach($package_name in $package_name_list)
         # NI Package Manager Layer
         $package_version = $nipm_version
         Write-Output "Building NI Package Manager Image Layer Version: $nipm_version."
-        docker build --rm -f .\$src_folder\$package_name\$package_version\Dockerfile `
+        docker build --rm -f ".\$src_folder\$package_name\$package_version\Dockerfile" `
                      -t $docker_user/${package_name}:$package_version `
                      ".\$src_folder\$package_name\$package_version"
-        #Write-Output "Pushing NI Package Manager Image..."
+        Write-Output "Pushing NI Package Manager Image..."
         #docker image push --all-tags $docker_user/nipm
     }else {
         foreach($package_bitness in $package_bitness_list)
@@ -54,8 +69,26 @@ foreach($package_name in $package_name_list)
                              ".\$src_folder\$package_name\$package_bitness\$labview_version"
             }
             
+            foreach($vianalyzer_version in $vianalyzer_version_list)
+            {
+                $package_version_bitness = "$vianalyzer_version-$package_bitness"
+                Write-Output "Building $package_name Image Layer Version: $package_version_bitness."
+                docker build --rm -f .\$src_folder\$package_name\$package_bitness\$vianalyzer_version\Dockerfile `
+                             -t $docker_user/${package_name}:$package_version_bitness `
+                             ".\$src_folder\$package_name\$package_bitness\$vianalyzer_version"
+            }
+            
+            foreach($utf_version in $utf_version_list)
+            {
+                $package_version_bitness = "$utf_version-$package_bitness"
+                Write-Output "Building $package_name Image Layer Version: $package_version_bitness."
+                docker build --rm -f .\$src_folder\$package_name\$package_bitness\$utf_version\Dockerfile `
+                             -t $docker_user/${package_name}:$package_version_bitness `
+                             ".\$src_folder\$package_name\$package_bitness\$utf_version"
+            }
+
         }
-        #Write-Output "Pushing $docker_user/$package_name Image..."
+        Write-Output "Pushing $docker_user/$package_name Image..."
         #docker image push --all-tags $docker_user/$package_name
     }
 }
